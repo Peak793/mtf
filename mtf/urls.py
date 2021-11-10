82d60c2954240d6e685e90462e69b20a38375429
CHANWANIT NUCHYOO <63010224@KMITL.AC.TH>
2021-11-10 09:51:27 +0700

    add-models

--- mtf/urls.py
+++ mtf/urls.py
@@ -17,9 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from app import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.store, name='store'),
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout')
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
