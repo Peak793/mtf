3c138261534ea0ca6fc5ed4cd42d4cb1d06606a5
CHANWANIT NUCHYOO <63010224@KMITL.AC.TH>
2021-11-10 10:55:02 +0700

    update

---
+++ urls.py
@@ -0,0 +1,39 @@
+"""mtf URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from app import views
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.store, name='store'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('fff/', views.fff, name='fff'),
+    path('shirt/', views.shirt, name='shirt'),
+    path('jens/', views.jens, name='jens'),
+    path('pong/', views.pong, name='pong'),
+    path('shose/', views.shose, name='shose'),
+    path('acc/', views.acc, name='acc'),
+]
+
+<<<<<<< HEAD
+=======
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+>>>>>>> 82d60c2954240d6e685e90462e69b20a38375429
